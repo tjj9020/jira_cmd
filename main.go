21caab828bdc05e9afa4d467927c2f31ace17248
Tim Johnson <6540172+tjj9020@users.noreply.github.com>
2019-08-12 13:21:11 -0400

    cleanup main.go

--- main.go
+++ main.go
@@ -3,18 +3,6 @@ package main
 import "github.com/tjj9020/jira_cmd/cmd"
 
 func main() {
-	// config, err := config.GetConfig()
-	// if err != nil {
-	// 	fmt.Println("Could not get config")
-	// }
 
-	// comment := "test comment"
-	// ticket := "PBT-15163"
-	// jiraComment := jira.NewJiraComment(comment, ticket, config)
-	// response, err := jiraComment.AddComment()
-	// if err != nil {
-	// 	fmt.Printf("There was an error, %v", err)
-	// }
-	// fmt.Println(response)
 	cmd.Execute()
 }
